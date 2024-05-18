bfeb5628b2a6cc96065b22c4a8f25d7658654f28
Jeeva <jeeva.deee111@gmail.com>
2024-05-18 09:25:50 +0530

    adding menu option

--- lib/home.dart
+++ lib/home.dart
@@ -1,19 +1,44 @@
 import 'package:flutter/material.dart';
 
+interface class IMenuOption {
+  final String title;
+  final VoidCallback callback;
+  const IMenuOption({required this.title, required this.callback});
+}
+
+List<IMenuOption> options = [
+  IMenuOption(title: "Play", callback: () {}),
+  IMenuOption(title: "Two Players", callback: () {})
+];
+
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ColoredBox(color: Colors.blueGrey, child: ListView.builder(itemBuilder: (context, index) => MenuOption(),),);
+    return Scaffold(
+      backgroundColor: Colors.blueGrey,
+      body: Center(
+        child: ListView.builder(
+              shrinkWrap: true,
+              physics: const NeverScrollableScrollPhysics(),
+              itemCount: options.length,
+              itemBuilder: (context, index) => MenuOption(option: options[index],),
+            ),
+      )
+    );
   }
 }
 
 class MenuOption extends StatelessWidget {
-  const MenuOption({super.key});
+  final IMenuOption option;
+  const MenuOption({super.key, required this.option});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SizedBox(
+      height: 50,
+      child: Text(option.title),
+    );
   }
 }
